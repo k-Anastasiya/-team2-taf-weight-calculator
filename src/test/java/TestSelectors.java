b95ef2703d013aecd0bbc4ca50a3cac1bebc0282
Kirill Vasilenko <wetanos@gmail.com>
2022-07-04 21:18:56 +0300

    remove: deleting extra files

--- src/test/java/TestSelectors.java
+++
@@ -1,9 +0,0 @@
-public class TestSelectors {
-    public static final String WEBSITEURL = "https://svyatoslav.biz/testlab/wt/";
-    public static final String NAMEXPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
-    public static final String HEIGHTXPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
-    public static final String WEIGHTXPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
-    public static final String MALERADIOBUTTONXPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[1]";
-    public static final String CALCULATEBUTTONXPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
-    public static final String ERRORMESSAGECSS = "form[method='post'] > table > tbody > tr:nth-of-type(1) > td";
-}
