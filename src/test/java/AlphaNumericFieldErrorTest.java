2cc0f375f4bb039e50aa561e7ef06ab332bbef16
Kirill Vasilenko <wetanos@gmail.com>
2022-07-04 21:40:29 +0300

    test: adding new test for alphanumeric values

---
+++ src/test/java/AlphaNumericFieldErrorTest.java
@@ -0,0 +1,37 @@
+import org.junit.*;
+import org.openqa.selenium.By;
+
+
+public class AlphaNumericFieldErrorTest extends WebDriverSettings {
+    @Test
+    public void testInvalidHeightField() {
+        // Given
+        driver.get(TestSelectors.WEBSITEURL);
+        // When
+        driver.findElement(By.xpath(TestSelectors.NAMEXPATH)).sendKeys("Кирилл");
+        driver.findElement(By.xpath(TestSelectors.HEIGHTXPATH)).sendKeys("180привет");
+        driver.findElement(By.xpath(TestSelectors.WEIGHTXPATH)).sendKeys("80");
+        driver.findElement(By.xpath(TestSelectors.MALERADIOBUTTONXPATH)).click();
+        driver.findElement(By.xpath(TestSelectors.CALCULATEBUTTONXPATH)).click();
+        // Then
+        String result = driver.findElement(By.cssSelector(TestSelectors.ERRORMESSAGECSS)).getText();
+        System.out.println(result);
+        Assert.assertEquals("Рост должен быть в диапазоне 50-300 см.", result);
+    }
+    @Test
+    public void testInvalidWeightField() {
+        // Given
+        driver.get(TestSelectors.WEBSITEURL);
+        // When
+        driver.findElement(By.xpath(TestSelectors.NAMEXPATH)).sendKeys("Кирилл");
+        driver.findElement(By.xpath(TestSelectors.HEIGHTXPATH)).sendKeys("180");
+        driver.findElement(By.xpath(TestSelectors.WEIGHTXPATH)).sendKeys("80привет");
+        driver.findElement(By.xpath(TestSelectors.MALERADIOBUTTONXPATH)).click();
+        driver.findElement(By.xpath(TestSelectors.CALCULATEBUTTONXPATH)).click();
+        // Then
+        String result = driver.findElement(By.cssSelector(TestSelectors.ERRORMESSAGECSS)).getText();
+        System.out.println(result);
+        Assert.assertEquals("Вес должен быть в диапазоне 3-500 кг.", result);
+    }
+
+}
