d1b5ca71b27be95a299449311b36b449bc2ced26
Kirill Vasilenko <wetanos@gmail.com>
2022-07-04 21:13:54 +0300

    feat: new web driver settings file for test preparations

--- src/test/java/AlphaNumericFieldErrorTest.java
+++ src/test/java/AlphaNumericFieldErrorTest.java
@@ -1,27 +1,14 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class WeightCalculatorTest {
-    WebDriver driver;
 
-    @Before
-    public void beforeClass() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
+public class WeightCalculatorTest extends WebDriverSettings {
     @Test
     public void testInvalidHeightField() {
         // Given
         driver.get(TestSelectors.WEBSITEURL);
         // When
-        WebElement actualName = driver.findElement(By.xpath(TestSelectors.NAMEXPATH));
-        actualName.sendKeys("Кирилл");
+        driver.findElement(By.xpath(TestSelectors.NAMEXPATH)).sendKeys("Кирилл");
         driver.findElement(By.xpath(TestSelectors.HEIGHTXPATH)).sendKeys("180привет");
         driver.findElement(By.xpath(TestSelectors.WEIGHTXPATH)).sendKeys("80");
         driver.findElement(By.xpath(TestSelectors.MALERADIOBUTTONXPATH)).click();
@@ -30,11 +17,6 @@ public class WeightCalculatorTest {
         String result = driver.findElement(By.cssSelector(TestSelectors.ERRORMESSAGECSS)).getText();
         System.out.println(result);
         Assert.assertEquals("Рост должен быть в диапазоне 50-300 см.", result);
-    }
-
-    @After
-    public void afterClass() {
-        driver.quit();
     }
 
 }
