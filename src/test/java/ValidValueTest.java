daa4d2d050746f222c2739b04b686eb752c8f0bd
Anastasiya Karpovich <nastasya.karpovich@gmail.com>
2022-07-05 10:54:32 +0300

    feat: add new TestClass

---
+++ src/test/java/ValidValueTest.java
@@ -0,0 +1,37 @@
+import org.junit.*;
+import org.openqa.selenium.By;
+
+
+public class ValidValueTest extends WebDriverSettings {
+    @Test
+    public void testValidMinValue() {
+        // Given
+        driver.get(TestSelectors.WEBSITEURL);
+        // When
+        driver.findElement(By.xpath(TestSelectors.NAMEXPATH)).sendKeys("Кирилл");
+        driver.findElement(By.xpath(TestSelectors.HEIGHTXPATH)).sendKeys("50");
+        driver.findElement(By.xpath(TestSelectors.WEIGHTXPATH)).sendKeys("3");
+        driver.findElement(By.xpath(TestSelectors.MALERADIOBUTTONXPATH)).click();
+        driver.findElement(By.xpath(TestSelectors.CALCULATEBUTTONXPATH)).click();
+        // Then
+        String result = driver.findElement(By.cssSelector(TestSelectors.ERRORMESSAGECSS)).getText();
+        System.out.println(result);
+        Assert.assertEquals("Слишком малая масса тела.", result);
+    }
+    @Test
+    public void testValidMaxValue() {
+        // Given
+        driver.get(TestSelectors.WEBSITEURL);
+        // When
+        driver.findElement(By.xpath(TestSelectors.NAMEXPATH)).sendKeys("Кирилл");
+        driver.findElement(By.xpath(TestSelectors.HEIGHTXPATH)).sendKeys("300");
+        driver.findElement(By.xpath(TestSelectors.WEIGHTXPATH)).sendKeys("500");
+        driver.findElement(By.xpath(TestSelectors.MALERADIOBUTTONXPATH)).click();
+        driver.findElement(By.xpath(TestSelectors.CALCULATEBUTTONXPATH)).click();
+        // Then
+        String result = driver.findElement(By.cssSelector(TestSelectors.ERRORMESSAGECSS)).getText();
+        System.out.println(result);
+        Assert.assertEquals("Значительный избыток массы тела, тучность.", result);
+    }
+
+}
