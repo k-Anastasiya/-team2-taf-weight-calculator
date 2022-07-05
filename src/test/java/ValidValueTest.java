035d7d4ac2df31a1d9d999f8d8228e6e4f4ad159
Anastasiya Karpovich <nastasya.karpovich@gmail.com>
2022-07-05 13:22:10 +0300

    fix test

--- src/test/java/ValidValueTest.java
+++ src/test/java/ValidValueTest.java
@@ -14,7 +14,7 @@ public class ValidValueTest extends WebDriverSettings {
         driver.findElement(By.xpath(TestSelectors.MALERADIOBUTTONXPATH)).click();
         driver.findElement(By.xpath(TestSelectors.CALCULATEBUTTONXPATH)).click();
         // Then
-        String result = driver.findElement(By.cssSelector(TestSelectors.ERRORMESSAGECSS)).getText();
+        String result = driver.findElement(By.Xpath(TestSelectors.ERRORMESSAGECSS)).getText();
         System.out.println(result);
         Assert.assertEquals("Слишком малая масса тела.", result);
     }
@@ -29,7 +29,7 @@ public class ValidValueTest extends WebDriverSettings {
         driver.findElement(By.xpath(TestSelectors.MALERADIOBUTTONXPATH)).click();
         driver.findElement(By.xpath(TestSelectors.CALCULATEBUTTONXPATH)).click();
         // Then
-        String result = driver.findElement(By.cssSelector(TestSelectors.ERRORMESSAGECSS)).getText();
+        String result = driver.findElement(By.Xpath(TestSelectors.ERRORMESSAGECSS)).getText();
         System.out.println(result);
         Assert.assertEquals("Значительный избыток массы тела, тучность.", result);
     }
